12a84e47d34f484967783ff39c656add580bd4d8
star <star@starlancer.io>
2025-05-02 15:50:39 -0700

    .

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,7 +1,7 @@
 import { Moon, Sun } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useTheme } from "@/hooks/use-theme";
-
+r
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
 
