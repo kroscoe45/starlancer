3bf5d80a13fd946c48b70f30e33c4ac2df3cad4f
star <star@starlancer.io>
2025-05-11 01:42:09 -0700

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { ThemeProvider } from './components/ThemeProvider'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ThemeProvider } from "./components/ThemeProvider";
 
-document.documentElement.classList.add('dark');
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="starlancer-theme">
       <App />
     </ThemeProvider>
   </StrictMode>,
-)
+);
+
